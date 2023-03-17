a397e4eaa2be66768b52862a76f07735fb38b589
Nguyễn Đức Duy <82205076+Lighty55@users.noreply.github.com>
2023-03-17 10:03:22 +0700

    Create dag-test.py

---
+++ dag-test.py
@@ -0,0 +1,32 @@
+import os
+from datetime import timedelta, datetime
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+
+default_args={
+   'depends_on_past': False,
+   'email': ['abcd@gmail.com'],
+   'email_on_failure': False,
+   'email_on_retry': False,
+   'retries': 1,
+   'retry_delay': timedelta(minutes=5)
+}
+dag = DAG(
+   'dag-spark-hdfs',
+   default_args={'max_active_runs': 1},
+   description='submit spark-pi as sparkApplication on kubernetes',
+   schedule_interval=timedelta(days=1),
+   start_date=datetime(2023, 3, 9),
+   catchup=False,
+)
+
+t1 = SparkKubernetesOperator(
+   task_id='hdfs-write',
+   retries=1,
+   application_file="write-hdfs.yaml",
+   namespace="default",
+   kubernetes_conn_id="myk8s",
+   do_xcom_push=True,
+   dag=dag,
+)
