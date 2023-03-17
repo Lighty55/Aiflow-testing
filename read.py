035fe621d9f956069261fbe43ac5911cbdb0cf6c
Nguyễn Đức Duy <82205076+Lighty55@users.noreply.github.com>
2023-03-17 15:44:39 +0700

    Create read.py

---
+++ read.py
@@ -0,0 +1,31 @@
+from pyspark.sql import SparkSession
+from textwrap import dedent
+import pendulum
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+
+with DAG(
+    'dag_example_read',
+    default_args={'retries': 0},
+    description="example",
+    schedule_interval=None,
+    start_date=pendulum.datetime(2022, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["example"],
+) as dag:
+    dag.doc_md = __doc__
+
+    def hdfs_example(**kwargs):
+        spark = SparkSession.builder.appName("PySpark Example - Read Parquet").getOrCreate()
+        spark.read.parquet("hdfs://namenode:9000/user/hadoop/example1").show()
+
+    example_read = PythonOperator(
+        task_id='example_read',
+        python_callable=hdfs_example,
+    )
+    example_read.doc_md = dedent(
+        """ extract task
+            extract data
+        """
+    )
